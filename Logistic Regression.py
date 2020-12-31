2dfefefc07f8425af4f551a60dabd9644f5954bd
Donga911 <Donga911@users.noreply.github.com>
2020-12-31 17:29:05 +0200

    Logistic Regression

---
+++ Logistic Regression.py
@@ -0,0 +1,31 @@
+from sklearn.linear_model import LogisticRegression
+from ClassficationDataProcessing import *
+from sklearn.metrics import  accuracy_score
+import time
+
+
+start_trainingtime=time.time()
+#Train Model
+LR_Model=LogisticRegression(max_iter=4790)
+
+#Model Fitting
+LR_Model.fit(X_train,y_train)
+
+end_trainigTime=time.time()
+#count_training_time
+trainingtime=end_trainigTime-start_trainingtime
+
+#prediction
+start_TestTime=time.time()
+
+LR_Predict=LR_Model.predict(X_test)
+
+end_TestTime=time.time()
+#count_test_time
+total_TestTime=end_TestTime-start_TestTime
+
+#Print
+LR_Accuracy=accuracy_score(y_test,LR_Predict)
+print("Accuracy is : ",LR_Accuracy)
+print("training time is: ",trainingtime)
+print("total test time: ",total_TestTime)
