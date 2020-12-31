d8c4db3048d6348a92768b8f83fd0355676d6106
Waleed Ehab Badr <waleedehab99@gmail.com>
2020-12-31 02:53:36 +0200

    Decision Tree Implemented

---
+++ DecisionTreeClassifier.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn import metrics
+import pickle
+from ClassficationDataProcessing import *
+
+DTclf = DecisionTreeClassifier(criterion="entropy", max_depth=3)
+DTclf = DTclf.fit(X_train,y_train)
+y_pred = DTclf.predict(X_test)
+result = metrics.accuracy_score(y_test, y_pred)
+print("Accuracy:",result)
+
